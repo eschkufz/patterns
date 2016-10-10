7483e3231845635e832bfb43381047b98a7bcdfd
eric schkufza <eric.schkufza@gmail.com>
2016-10-10 14:12:42 -0700

    Update singleton.h

--- include/singleton.h
+++ include/singleton.h
@@ -11,7 +11,6 @@ struct Singleton {
   Singleton(const Singleton& rhs) = delete;
   Singleton(const Singleton&& rhs) = delete;
   Singleton& operator=(const Singleton& rhs) = delete;
-  Singleton&& operator==(const Singleton&& rhs) = delete;
 
   template <typename ...Args>
   static T& get(Args... args) {
